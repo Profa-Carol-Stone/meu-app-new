b1defd9531312fe13e615b580a5636a33739f8e0
Profa-Carol-Stone <92635992+Profa-Carol-Stone@users.noreply.github.com>
2024-10-19 10:23:31 -0300

    Add files via upload

---
+++ src/Relogio.js
@@ -0,0 +1,10 @@
+function Relogio() {
+    return (
+      <>
+          <h3>Meu Relógio</h3>
+      </>
+    );
+  }
+  
+  export default Relogio;
+  
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MinhasRotas from './minhasRotas';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <MinhasRotas />
+);
+
+
